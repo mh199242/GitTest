3d8ecd9ff0ee01d08888f75f8ae7631b95ead700
mh <mh199242@163.com>
2017-08-03 16:23:04 +0800

    改成word

--- app/src/main/java/com/attackt/gittest/MainActivity.java
+++ app/src/main/java/com/attackt/gittest/MainActivity.java
@@ -2,8 +2,10 @@ package com.attackt.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
