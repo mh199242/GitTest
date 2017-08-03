37c03e2b4b6100ee73eff61232c5d00139cda917
mh <mh199242@163.com>
2017-08-03 18:15:06 +0800

    加button

--- app/src/main/java/com/attackt/gittest/MainActivity.java
+++ app/src/main/java/com/attackt/gittest/MainActivity.java
@@ -2,10 +2,12 @@ package com.attackt.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
